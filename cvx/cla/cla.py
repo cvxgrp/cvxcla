572789f2080ac2f60f8b133b770255c218a70860
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-07-29 07:59:40 -0700

    Update cla.py

--- cvx/cla/cla.py
+++ cvx/cla/cla.py
@@ -40,8 +40,8 @@ class TurningPoint:
     def blocked_assets(self):
         f = np.array([False for _ in self.weights])
         f[self.free_assets] = True
-        return np.where(~f)[0]
-
+        return list(np.where(~f)[0])
+        
     @staticmethod
     def construct(mean, lower_bounds, upper_bounds, covariance):
         first = init_algo(
