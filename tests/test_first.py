6c980d059193baf679be6415756ce4883e648e98
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-08-03 13:44:15 -0700

    remove test for init algo lp

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from cvx.cla._first import init_algo, init_algo_lp
+from cvx.cla._first import init_algo
 
 
 def test_init_algo():
@@ -48,15 +48,3 @@ def test_order():
     mean = np.array([1.0, 1.0, 1.0])
     order = np.argsort(mean)[::-1]
     assert np.alltrue(np.sort(mean)[::-1] == mean[order])
-
-
-def test_lp():
-    mean = np.array([1.0, 2.0, 3.0])
-    lower_bound = np.array([0.0, 0.0, 0.0])
-    upper_bound = np.array([0.4, 0.4, 0.4])
-    next = init_algo_lp(mean=mean, lower_bounds=lower_bound, upper_bounds=upper_bound)
-
-    assert np.allclose(next.weights, [0.2, 0.4, 0.4])
-    assert next.lamb == np.inf
-    assert np.allclose(next.free, [True, False, False])
-    assert np.allclose(next.mean, 2.2)
