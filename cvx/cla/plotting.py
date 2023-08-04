638027c0656eb0227827b0efab7a551ae6d2a20c
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-08-04 12:26:26 -0700

    plotting with plotly

--- cvx/cla/plotting.py
+++ cvx/cla/plotting.py
@@ -1,19 +1,31 @@
 from typing import List
 
-from matplotlib import pyplot as plt
 from cvx.cla import Frontier
-
-
+import plotly.graph_objects as go
 def plot_efficient_frontiers(frontiers: List[Frontier]) -> None:
+    # Create an empty list to store the traces
+    traces = []
 
-    plt.figure()
-
+    # Loop through each set of y-values
     for frontier in frontiers:
-        plt.plot(frontier.variance, frontier.returns, label=frontier.name)
+        # Create a trace for each curve
+        trace = go.Scatter(
+            x=frontier.variance,
+            y=frontier.returns,
+            name=frontier.name,
+        )
+        # Add the trace to the list
+        traces.append(trace)
 
-    plt.title("Efficient Frontier")
-    plt.xlabel("variance")
-    plt.ylabel("mu")
+    # Create the layout object
+    layout = go.Layout(
+        title='Efficient Frontier(s)',
+        xaxis=dict(title='\\mu'),
+        yaxis=dict(title='Variance'),
+    )
 
-    plt.legend()
-    plt.show()
+    # Create the figure object
+    fig = go.Figure(data=traces, layout=layout)
+
+    # Open the figure
+    fig.show()
