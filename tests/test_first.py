19ca44f7fe0c14f769ccdadecfaca5b4bdc4be09
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-08-05 19:39:23 -0700

    testing starting point

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pytest
 
-from cvx.cla._first import init_algo
+from cvx.cla.first import init_algo
 
 
-def test_init_algo():
+@pytest.mark.parametrize("n", [5, 20, 100, 1000])
+def test_init_algo(n):
+    mean = np.random.randn(n)
+    lB = np.zeros(n)
+    uB = np.random.rand(n)
+
+    first = init_algo(mean=mean, lower_bounds=lB, upper_bounds=uB)
+
+    assert np.sum(first.free) == 1
+    assert np.sum(first.weights) == pytest.approx(1.0)
+
+def test_small():
     mean = np.array([1.0, 2.0, 3.0])
     lower_bound = np.array([0.0, 0.0, 0.0])
     upper_bound = np.array([0.4, 0.4, 0.4])
@@ -13,19 +24,23 @@ def test_init_algo():
     assert np.allclose(next.weights, [0.2, 0.4, 0.4])
     assert next.lamb == np.inf
     assert np.allclose(next.free, [True, False, False])
-    assert np.allclose(next.mean, 2.2)
 
 
-def test_init_algo_border():
-    mean = np.array([1.0, 1.0, 1.0])
-    lower_bounds=np.array([0.0, 0.0, 0.0])
-    upper_bounds=np.array([1.0, 1.0, 1.0])
+@pytest.mark.parametrize("n", [5, 20, 100, 1000])
+def test_init_algo(n):
+    mean = np.ones(n)
+    lower_bounds=np.zeros(n) #array([0.0, 0.0, 0.0])
+    upper_bounds=np.ones(n) #array([1.0, 1.0, 1.0])
     tp = init_algo(mean=mean, lower_bounds=lower_bounds, upper_bounds=upper_bounds)
 
-    assert np.allclose(tp.weights, [0.0, 0.0, 1.0])
+    b = np.zeros(n)
+    b[0] = 1.0
+    assert np.allclose(tp.weights, b)
     assert tp.lamb == np.inf
-    assert np.allclose(tp.free, [False, False, True])
-    assert np.allclose(tp.mean, 1.0)
+
+    b = np.full_like(mean, False)
+    b[0] = True
+    assert np.allclose(tp.free, b)
 
 
 def test_no_free_asset():
@@ -38,13 +53,25 @@ def test_no_free_asset():
     ):
         init_algo(mean=mean, lower_bounds=lb, upper_bounds=ub)
 
-    # assert np.allclose(tp.weights, [0.2, 0.2, 0.2])
-    # assert tp.lamb == np.inf
-    # assert np.allclose(tp.free, [False, False, False])
-    # assert np.allclose(tp.mean, 1.2)
-
 
 def test_order():
     mean = np.array([1.0, 1.0, 1.0])
     order = np.argsort(mean)[::-1]
     assert np.alltrue(np.sort(mean)[::-1] == mean[order])
+
+
+def test_lb_ub_mixed():
+    uB = np.zeros(3)
+    lB = np.ones(3)
+    mean = np.ones(3)
+
+    with pytest.raises(ValueError):
+        init_algo(mean=mean, lower_bounds=lB, upper_bounds=uB)
+
+def test_no_fully_invested():
+    uB = 0.2 * np.ones(3)
+    lB = np.zeros(3)
+    mean = np.ones(3)
+
+    with pytest.raises(ValueError):
+        init_algo(mean=mean, lower_bounds=lB, upper_bounds=uB)
