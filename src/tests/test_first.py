43a0a4ac21d6ce18d43cbf61863fec1bf2eef32a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-25 12:22:45 +0200

    testing, testing, testing

--- src/tests/test_first.py
+++ src/tests/test_first.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from cvxcla.first import init_algo, _free
+from cvxcla.first import _free, init_algo
 from cvxcla.types import TurningPoint
 
 
